c260b5349b07039f4c4c346eadc95b26f258a993
rakurai <rakurai@client.rakurai-qv7239.bdslab-pg0.clemson.cloudlab.us>
2015-07-08 16:40:05 -0400

    debugging printfs

--- client.c
+++ client.c
@@ -36,9 +36,13 @@ int main(int argc, char *argv[])
     else
         echoServPort = 7;  /* 7 is the well-known port for the echo service */
 
+    printf("Opening socket to %s on port %d... ", servIP, echoServPort);
+
     /* Create a reliable, stream socket using TCP */
     if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket() failed");
+
+    printf("ok.\n");
 
     /* Construct the server address structure */
     memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
@@ -46,19 +50,25 @@ int main(int argc, char *argv[])
     echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
     echoServAddr.sin_port        = htons(echoServPort); /* Server port */
 
+    printf("Connecting to server... ");
+
     /* Establish the connection to the echo server */
     if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
         DieWithError("connect() failed");
 
+    printf("ok.\n");
     echoStringLen = strlen(echoString);          /* Determine input length */
 
+    printf("Sending '%s'... ", echoString);
     /* Send the string to the server */
     if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
         DieWithError("send() sent a different number of bytes than expected");
 
+    printf("ok.\n");
+
     /* Receive the same string back from the server */
     totalBytesRcvd = 0;
-    printf("Received: ");                /* Setup to print the echoed string */
+    printf("Receiving response... ");                /* Setup to print the echoed string */
     while (totalBytesRcvd < echoStringLen)
     {
         /* Receive up to the buffer size (minus 1 to leave space for
